bb3bfc3d99eab960b18b80405c8f03daeb7d3df7
NemaDev <petermannfelipe@gmail.com>
2023-04-17 22:14:26 -0400

    wdad

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://github.com/nemaDev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/nemadev.gitbub.io',
+  baseUrl: '/nemadev',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
